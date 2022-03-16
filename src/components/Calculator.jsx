d205ca5c5014667ec3ffda377813e581a24109cc
Steve <stevenjchang@gmail.com>
2022-03-15 20:08:01 -0700

    Initial commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,201 @@
+import { useEffect, useState } from "react";
+import { FiEdit } from "react-icons/fi";
+import { GrEdit } from "react-icons/gr";
+import {
+  NumberInput,
+  Tag,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Divider,
+  IconButton,
+  Checkbox,
+  Text,
+  TagRightIcon
+} from "@chakra-ui/react";
+
+import MyModal from "./MyModal";
+import "./styles.css";
+import ChangeNameModal from "./ChangeNameModal";
+
+const tagColors = [
+  // 'whiteAlpha',
+  // 'blackAlpha',
+  // 'gray',
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "teal",
+  "blue",
+  "cyan",
+  "purple",
+  "pink",
+  "linkedin",
+  "facebook",
+  "messenger",
+  "whatsapp",
+  "twitter",
+  "telegram"
+];
+
+// const colors2 = [
+//   hex:
+// ]
+
+const initialPersons = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+
+const initialReceipt = [
+  {
+    price: 1000,
+    items: []
+  },
+  {
+    price: 2000
+  },
+  {
+    price: 2000
+  },
+  {
+    price: 4000
+  }
+];
+
+const InputGroup = ({ persons, foodItem, setPersons }) => {
+  return (
+    <>
+      <div className="my-8">
+        <MyModal />
+        <NumberInput
+          maxW="100px"
+          size="sm"
+          variant="filled"
+          defaultValue={15}
+          precision={2}
+          step={1}
+          min={0}
+          _active={{ bg: "green.300" }}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+
+        <div className="flex flex-wrap pl-2">
+          {persons.map((p, i) => (
+            <div key={i} className="persons-container my-2 flex items-center">
+              <Checkbox key={i} className="mr-1" defaultChecked />
+              {/* <Tag size="sm" key={i} idx={i} colorScheme={tagColors[i]}>
+                {`Person ${i + 1}`}
+              </Tag> */}
+              <span className="text__person--name">{`Person ${i + 1}`}</span>
+              <Text fontSize="xs" className="inline mx-1">
+                pays
+              </Text>
+
+              <span className="text__price">$5</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Divider />
+    </>
+  );
+};
+
+const Calculator = () => {
+  const [numberOfPersons, setNumberOfPersons] = useState(4);
+  const [persons, setPersons] = useState(initialPersons);
+  const [foodItems, setFood] = useState(initialReceipt);
+
+  useEffect(() => {
+    console.log("persons", persons);
+  }, [persons]);
+
+  const handleChange = (val) => {
+    const newNum = Number(val);
+
+    if (newNum < persons.length) {
+      setPersons((prevState) => {
+        const newState = prevState.slice(0, prevState.length - 1);
+        return newState;
+      });
+    } else {
+      setPersons((prevState) => {
+        console.log("prevState", prevState);
+        const newState = [...prevState, { id: val }];
+        console.log("new", newState);
+        return newState;
+      });
+    }
+
+    setNumberOfPersons(newNum);
+  };
+
+  const handleNameChange = (idx, name) => {
+    setPersons((prev) => {
+      prev[idx].name = name;
+      console.log("prev", prev);
+      return prev;
+    });
+  };
+
+  return (
+    <section id="taxcalc-section" className="mx-auto">
+      <h1>{JSON.stringify(persons, null, 4)}</h1>
+      <div className="">
+        <div className="flex flex-col items-center">
+          <h2 className="my-4">How many people?</h2>
+          <NumberInput
+            size="md"
+            maxW="70px"
+            defaultValue={numberOfPersons}
+            onChange={handleChange}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </div>
+        <div id="badge-container" className="flex justify-center my-8">
+          {persons.map((p, i) => {
+            return (
+              <ChangeNameModal idx={i} handleNameChange={handleNameChange} />
+            );
+
+            // return (
+            //   <Tag key={i} colorScheme={tagColors[i]} onClick={onChange}>
+            //     {`Person ${i + 1}`}
+            //     <TagRightIcon boxSize="12px" as={GrEdit} />
+            //   </Tag>
+            // );
+          })}
+        </div>
+      </div>
+
+      <main>
+        <div className="">
+          <h1 className="text-center">shared items</h1>
+          <hr />
+          <br />
+          <br />
+          {foodItems.map((item, i) => (
+            <InputGroup
+              key={i}
+              persons={persons}
+              foodItem={item}
+              setPersons={setPersons}
+            />
+          ))}
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default Calculator;
---
+++ src/components/ChangeNameModal.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Text,
+  useDisclosure,
+  Tag,
+  TagRightIcon,
+  Input
+} from "@chakra-ui/react";
+import { GrEdit } from "react-icons/gr";
+
+const ChangeNameModal = ({ propsIsOpen, idx, handleNameChange }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [value, setValue] = useState(null);
+
+  useEffect(() => {
+    if (propsIsOpen) {
+      onOpen();
+    } else {
+      onClose();
+    }
+  }, [propsIsOpen, onOpen, onClose]);
+
+  const handleChange = (event) => setValue(event.target.value);
+
+  const handleSubmit = () => {
+    handleNameChange(idx, value);
+    onClose();
+  };
+  return (
+    <>
+      {/* <Button onClick={onOpen}>Open Modal</Button> */}
+      <Tag onClick={onOpen}>
+        {`Person ${idx + 1}`}
+        <TagRightIcon boxSize="12px" as={GrEdit} />
+      </Tag>
+
+      <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text
+              fontWeight="bold"
+              mb="1rem"
+              onClick={() => handleNameChange(idx, "danking")}
+            >
+              Person
+            </Text>
+            <Input
+              variant="filled"
+              placeholder="Enter Name"
+              value={value}
+              onChange={handleChange}
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
+              Enter
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ChangeNameModal;
